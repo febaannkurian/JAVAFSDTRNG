2dc3299e12a0b13bac8cbbe35c381cb9e6de132a
febaannkurian <69293142+febaannkurian@users.noreply.github.com>
2021-08-27 13:24:56 +0530

    Add files via upload

---
+++ Demo_Project1/Springexample2/src/main/java/com/ust/springboot/Emp.java
@@ -0,0 +1,68 @@
+package com.ust.springboot;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document
+public class Emp {
+	@Id
+	
+	private String id;
+
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String designation;
+	
+	private String Salary;
+	
+	public String getSalary() {
+		return Salary;
+	}
+	public void setSalary(String salary) {
+		Salary = salary;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
